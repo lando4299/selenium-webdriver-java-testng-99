72580c7b3b96c0d9db4571a96d207c2f4eb9f4c6
thuan29012003 <dovanthuan29012003@gmail.com>
2026-01-11 22:39:30 +0700

    29

--- selenium/webdriver/Topic_22_Random_Popup.java
+++ selenium/webdriver/Topic_22_Random_Popup.java
@@ -1,7 +1,10 @@
 package webdriver;
 
+import com.beust.ah.A;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -11,21 +14,59 @@ import java.time.Duration;
 public class Topic_22_Random_Popup {
     WebDriver driver;
 
-   @BeforeClass
-public void beforeClass(){
-    driver = new FirefoxDriver();
-    driver.manage().window().maximize();
-    driver.manage().timeouts(). implicitlyWait(Duration.ofSeconds(15));
-
-}
-
-    @Test
-    public void TC_01_() {
+    @BeforeClass
+    public void beforeClass(){
+        System.setProperty("webdriver.gecko.driver", "C:\\Windows\\System32\\geckodriver.exe"); // khác
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts(). implicitlyWait(Duration.ofSeconds(35));
 
     }
 
     @Test
-    public void TC_02_() {
+    public void TC_01_JavaCodeGeeks_NOT_IN_DOM() throws InterruptedException {
+        driver.get("https://www.javacodegeeks.com/");
+
+        By popupJavaCodeGeeks = By.xpath("//div[@data-title='Newsletter-Books Anime Brief - NEW'" +
+                "and not(contains(@style, 'display:none'))]");
+// Hiêển thị popup thì close
+        if (driver.findElements(popupJavaCodeGeeks).size() > 0
+                && driver.findElements(popupJavaCodeGeeks).get(0).isDisplayed()) // có trong  list popup và get ra cái đầu tiên có hiển thi trên UI
+
+        { System.out.println("_____________GO TO IF_________");
+            driver.findElement(By.xpath("//div[@data-title='Newsletter-Books Anime Brief - NEW' " +
+                    "and not(contains(@style,'display:none'))]//a[contains(@onclick, 'return lepopup_close')]")).click();
+            Thread.sleep(3000);
+            Assert.assertEquals(driver.findElements(popupJavaCodeGeeks).size(), 0);
+            // Popup sau khi click đóng ko còn trong HTML thì phải dùng  findElements va kiem tra size =0, nếu còn trong HTML thì dùng isDisplayed
+        }
+
+// Ko hiển thị thì đi tiếp
+        System.out.println("_____________NOTTTT GO TO IF_________");
+        driver.findElement(By.cssSelector("form#search>input#search-input")).sendKeys("Agile");
+        driver.findElement(By.cssSelector("button#search-submit>span.tie-icon-search")).click();
+        Thread.sleep(5000);
+
+        Assert.assertTrue(driver.findElement(By.cssSelector("header>h1.page-title")).isDisplayed());
+
+    }
+
+    @Test
+    public void TC_02_Withpopup_random_popup_IN_DOM() throws InterruptedException {
+        driver.get("https://dehieu.vn/");
+        By popupDeHieu = By.cssSelector("div.modal-content");
+        if (driver.findElements(popupDeHieu).size() > 0 && driver.findElements(popupDeHieu).get(0).isDisplayed())
+
+        { System.out.println("__________GO To IF__________");
+            driver.findElement(By.cssSelector("div.modal-content button.close")).click();
+
+            // Verify popup ko con hien thi
+            Assert.assertFalse(driver.findElement(popupDeHieu).isDisplayed());
+
+        }
+        System.out.println("_____________NOTTTT GO TO IF_________");
+        driver.findElement(By.cssSelector("")).click();
+
 
     }
 
@@ -37,6 +78,6 @@ public void beforeClass(){
     @AfterClass
     public void afterClass() {
 
-    driver.quit();
+        driver.quit();
     }
 }
---
+++ selenium/webdriver/Topic_23_Window_Tap.java
@@ -0,0 +1,138 @@
+package webdriver;
+
+import org.bouncycastle.util.Strings;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.Set;
+
+public class Topic_23_Window_Tap {
+    WebDriver driver;
+
+   @BeforeClass
+public void beforeClass(){
+    driver = new FirefoxDriver();
+    driver.manage().window().maximize();
+    driver.manage().timeouts(). implicitlyWait(Duration.ofSeconds(15));
+
+}
+
+    @Test
+    public void TC_01_Github() throws InterruptedException {
+       driver.get("https://automationfc.github.io/basic-form/index.html");
+       // Lấy ra windowId/ tabId mà driver đang dứng tại do
+        String githubWindowID = driver.getWindowHandle();
+        System.out.println("Page ID = "  + driver.getWindowHandle());
+        System.out.println("Page title = "  + driver.getTitle());
+        System.out.println("Current URL hien tai = "  + driver.getCurrentUrl());
+
+        // Click vao google link se mo sang tap moi ve mat business
+        // Nhung driver vẫn đứng taại tap ban dau Github
+        driver.findElement(By.xpath("//a[text()=\"GOOGLE\"]")).click();
+        Thread.sleep(3000);
+
+        // Switch qua tab Google
+        switchToWindowById(githubWindowID); //DA EXTRACT THANH 1 HAM BEN DUOI
+
+        // Kiem tra vi tri hien tai cua drive
+        System.out.println("Page ID = "  + driver.getWindowHandle());
+        System.out.println("Page title = "  + driver.getTitle());
+        System.out.println("Current URL hien tai = "  + driver.getCurrentUrl());
+
+        // Switch ve tap Github ban dau
+        String googleWindowID = driver.getWindowHandle(); // Lay ra ID cua tab google de truyen vao ham Switch
+
+        switchToWindowById(googleWindowID); //DA EXTRACT THANH 1 HAM BEN DUOI
+
+        //Ve Github tiep tuc chon facebook
+        driver.findElement(By.xpath("//a[text()=\"FACEBOOK\"]")).click();
+        Thread.sleep(3000);
+
+        //TH co 3 window
+        //B1: lay ra tat ca window ID
+        switchToWindowByTitle("Facebook – log in or sign up");
+        Thread.sleep(3000);
+
+        // Quay ve Github
+        switchToWindowByTitle("Selenium WebDriver");
+        Thread.sleep(3000);
+
+        //Ve Github tiep tuc chon Tiki
+        driver.findElement(By.xpath("//a[text()=\"TIKI\"]")).click();
+        Thread.sleep(3000);
+        //Switch to TIKI
+        switchToWindowByTitle("Tiki - Mua hàng online giá tốt, hàng chuẩn, ship nhanh");
+        Thread.sleep(3000);
+
+        //Lay ra het tat ca tap/window
+        closeWindowWithoutParent(githubWindowID); //DA EXTRACT THANH 1 HAM BEN DUOI
+
+        //switch vao tap cuoi cung con lai
+        driver.switchTo().window(githubWindowID);
+    }
+        // Dung voi tu 3 tab/window dung =2 van dung => dung luon cai nay
+    private void closeWindowWithoutParent(String githubWindowID) throws InterruptedException {
+        Set <String> allWindowIDs = driver.getWindowHandles();
+        //Dung vong lap duyet qua tung ID
+        for (String id : allWindowIDs){
+                if (!id.equals(githubWindowID)){
+                    // id ko dund thi switch vao window do va close
+                    driver.switchTo().window(id);
+                    Thread.sleep(3000);
+                    driver.close();
+                }
+            }
+    }
+    // Dung voi tu 3 tab/window dung =2 van dung => dung luon cai nay
+    private void switchToWindowByTitle(String expectedpageTitle) {
+        Set <String> allWindowIDs = driver.getWindowHandles();
+
+        //Dung vong lap duyet qua tung ID
+        for (String id : allWindowIDs) {
+            //Moi lan duyet se cho driver switch vao truoc roi get ra title va so sanh
+            driver.switchTo().window(id);
+            String pageTitle = driver.getTitle();
+
+            // so sanh page title
+            if (pageTitle.equals(expectedpageTitle)){
+                break;
+                // neu dung thi dung vong lap va driver dung o page do luon
+            }
+        }
+    }
+
+    // Chi dung TH handle voi 2 tab/ window, nhieu hon la sai
+    private void switchToWindowById(String windowId) {
+       //Lay ra tat ca window/ tab hien tai
+        Set<String> allWindowIDs = driver.getWindowHandles();
+        // Duyet lan luot tung id mot
+        for (String id : allWindowIDs) {
+            if (!id.equals(windowId)
+            ) {
+
+                driver.switchTo().window(id);
+            }
+        }
+    }
+
+    @Test
+    public void TC_02_() {
+
+    }
+
+    @Test
+    public void TC_03_() {
+
+
+    }
+    @AfterClass
+    public void afterClass() {
+
+    driver.quit();
+    }
+}
